2bd9ef3a42b809cdc9aa1edd7f5bc287bfbd9fe1
mohamad reza koohkan <mohamad_koohkan@icloud.com>
2023-02-17 08:54:54 +0300

    Update home page to say greeting according to user timezone

--- src/common/scripts/ip.js
+++ src/common/scripts/ip.js
@@ -2,14 +2,33 @@ const DEFAULT_IP = "";
 const ENDPOINT_URL = "https://api.ipgeolocation.io/getip";
 
 async function getIP() {
-    let response = await fetch(ENDPOINT_URL);
-    let data = await response.json();
-    let ip = await data.ip;
-    return ip;
+  const response = await fetch(ENDPOINT_URL);
+  const data = await response.json();
+  const ip = await data.ip;
+  return ip;
 }
 
 function updateUI(ip) {
-    document.getElementById("ip").innerHTML = `Hi ${ip}`;
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const component = document.getElementById("ip");
+  const greeting = getGreeting(timeZone);
+  component.innerHTML = `${greeting} ${ip}`;
+}
+
+function getGreeting(timeZone) {
+  const timeZones = {
+    "Europe/Istanbul": "Merhaba",
+    "Europe/Madrid": "Hola",
+    "Europe/Rome": "Ciao",
+    "Europe/Paris": "Bonjour",
+    "Europe/Berlin": "Hallo",
+  };
+  var result = timeZones[timeZone];
+  console.log(result);
+  if (typeof result == "undefined") {
+    result = "Hi";
+  }
+  return result;
 }
 
 updateUI(DEFAULT_IP);
